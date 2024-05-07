af810f6f493a1af6d55802a9bee9bad908bb46b6
Maique Douglas <rmaique762@gmail.com>
2024-05-07 20:45:10 -0300

    Melhoria do código dos componentes Footer e categirues

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Categories.css'; 
-import Photo1 from '../../assets/images/categories1.png';
+import Photo1 from '../../assets/images/Categories1.png';
+import Photo2 from '../../assets/images/Categories2.png';
+import Photo3 from '../../assets/images/Categories3.png';
+import Photo4 from '../../assets/images/Categories4.png';
+import Photo5 from '../../assets/images/Categories5.png';
+import Photo6 from '../../assets/images/Categories6.png';
+
 
 
 function Categories() {
@@ -11,26 +17,26 @@ function Categories() {
                 <ul>
                     <li>
                         <img src={Photo1} alt="Categoria 1" />
-                        <span>Categoria 1</span>
+                        <span>Ursinhos</span>
                     </li>
                     <li>
-                        <img src={Photo1}alt="Categoria 2" />
-                        <span>Categoria 2</span>
+                        <img src={Photo2}alt="Categoria 2" />
+                        <span>Tricotin</span>
                     </li>
                     <li>
-                        <img src={Photo1} alt="Categoria 3" />
+                        <img src={Photo3} alt="Categoria 3" />
                         <span>Categoria 3</span>
                     </li>
                     <li>
-                        <img src={Photo1} alt="Categoria 4" />
+                        <img src={Photo4} alt="Categoria 4" />
                         <span>Categoria 4</span>
                     </li>
                     <li>
-                        <img src={Photo1} alt="Categoria 5" />
+                        <img src={Photo5} alt="Categoria 5" />
                         <span>Categoria 5</span>
                     </li>
                     <li>
-                        <img src={Photo1} alt="Categoria 6" />
+                        <img src={Photo6} alt="Categoria 6" />
                         <span>Categoria 6</span>
                     </li>
                 </ul>
